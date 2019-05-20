91ea851465cedfaa33c5816844c542f90ad12cf3
jturpijn <joshua_turpijn@hotmail.com>
2019-05-20 11:16:01 +0200

    Removed benchmark code from tl_mergesort

--- src/G2_PCM/TL_MergeSort.java
+++ src/G2_PCM/TL_MergeSort.java
@@ -15,9 +15,6 @@ public class TL_MergeSort {
         // unsorted array 
         array = new int[anArray.length]; array = anArray;
 
-        // set starting timer
-        // long start = System.currentTimeMillis();
-
         // split array in lower and higher half
         for (int i = 0; i < array.length; i++) {
             if(array[i] <= (array.length/2)) { low.add(array[i]); } 
@@ -29,12 +26,5 @@ public class TL_MergeSort {
         Thread t2 = new Thread(new TL_mergethreadHigh(high));
         t1.start(); t2.start();
         t1.join();  t2.join();
-        
-        // stop timer and output results
-        // long end = System.currentTimeMillis();
-        // System.out.println("Type: Threads & Locks MergeSort");
-        // System.out.println("\t Amount of elements: " + SIZE);
-        // System.out.println("\t Total time in milliseconds: " + (end - start));
-        // System.out.println("\t Sorted array : "); Utils.print(outputLow); Utils.print(outputHigh);
     }
 }
